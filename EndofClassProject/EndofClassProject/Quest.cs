8d97d341bb764892a218d93cceb61e8522af0193
jgay90 <19jgay90@gmail.com>
2016-11-29 09:46:30 -0500

    working on the quest class to work

---
+++ EndofClassProject/EndofClassProject/Quest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EndofClassProject
+{
+    class Quest
+    {
+        public string questName;
+        public string[] keywords;
+
+
+        public Quest()
+        {
+            questName = "Default Quest";
+            keywords = new string[5];
+            keywords[0] = "Hello";
+        }
+
+        public Quest(string name, string[] keys)
+        {
+            questName = name;
+            keywords = keys;
+        }
+
+        public void setQuestName(string name)
+        {
+            questName = name;
+
+        }
+        public void setKeywords(string[] keys)
+        {
+            keywords = keys;
+        }
+
+        public bool checkKeyword(string key,string[] keyword)
+        {
+           bool check = false;
+           int i = 0;
+            foreach (string x in  keywords)
+            {
+                if (x == keyword[i])
+                {
+
+                }
+                else
+                {
+                    i++;
+                }
+                return check;
+        }
+    }
+}
