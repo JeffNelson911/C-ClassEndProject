b1a630cdffe525d24f91aecebb6d5a457a941aca
Jeff Nelson <jefft@DESKTOP-EVD0388>
2016-11-30 15:35:28 -0500

    Made the Flower Quest. Someone else can fill in the details.

---
+++ EndofClassProject/EndofClassProject/FlowerQuest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EndofClassProject
+{
+    class FlowerQuest : Quest
+    {
+
+        public FlowerQuest()
+        {
+            questName = "Retrieve the Crysantheum";
+            keywords = new string[5];
+            keywords[0] = "flower";
+            keywords[1] = "crysantheum";
+            description = "We need the rare Crysantheum flower for reason.";
+            qId = 1;
+            prompt = "We need the Crysantheum flower. It is said to be a miracle cure that can solve anything. Can you find it for me?";
+            state = 0;
+        }
+
+    }
+}
