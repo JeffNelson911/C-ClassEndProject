8703278300dff545ddc374204578b9d58894a0f8
weilandr0843 <weilandr0843@ATC15601>
2016-12-06 11:34:52 -0500

    Commented the quests out.

--- EndofClassProject/EndofClassProject/BladeQuest.cs
+++ EndofClassProject/EndofClassProject/BladeQuest.cs
@@ -1,90 +1,91 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace EndofClassProject
-{
-    class BladeQuest : Quest
-    {
+//namespace EndofClassProject
+//{
+//    class BladeQuest : Quest
+//    {
 
-        private List<Item> inventory = new List<Item>();
+//        private List<Item> inventory = new List<Item>();
 
-        public BladeQuest()
-        {
-            questName = "Retrieve the Vorpal Blade";
-            keywords = new string[5];
-            keywords[0] = "blade";
-            keywords[1] = "crysantheum";
-            description = "We need the rare Vorpal Blade for reason.";
-            qId = 1;
-            prompt = "We need the Vorpal Blade. It is said to be a powerful weapon that can pierce any defense. Can you find it for me?";
-            state = 0;
-        }
 
-        public void copyInventory(List<Item> i)
-        {
-            inventory = i;
-        }
+//        public BladeQuest()
+//        {
+//            questName = "Retrieve the Vorpal Blade";
+//            keywords = new string[5];
+//            keywords[0] = "blade";
+//            keywords[1] = "crysantheum";
+//            description = "We need the rare Vorpal Blade for reason.";
+//            qId = 1;
+//            prompt = "We need the Vorpal Blade. It is said to be a powerful weapon that can pierce any defense. Can you find it for me?";
+//            state = 0;
+//        }
 
-        public override void checkQuest()
-        {
-            state = 0;
-            Item placeholder = null;
-            switch (state)
-            {
-                case 0:
-                    if (keywords.Contains("blade"))
-                    {
-                        state++;
-                    }
-                    break;
-                case 1:
-                    foreach (Item val in inventory)
-                    {
-                        if (val.Name.Equals("vorpal blade"))
-                        {
-                            placeholder = val;
-                            break;
-                        }
-                    }
-                    if (placeholder != null)
-                    {
-                        state++;
-                    }
-                    break;
-                case 2:
-                    if (hasGiven() == true) //if player uses give comand change given to 1 and return on hasGiven check true add Quest giver to the paramater in hasGiven
-                    {
+//        public void copyInventory(List<Item> i)
+//        {
+//            inventory = i;
+//        }
 
-                        //update journal to complete and award EXP
+//        public override void checkQuest()
+//        {
+//            state = 0;
+//            Item placeholder = null;
+//            switch (state)
+//            {
+//                case 0:
+//                    if (keywords.Contains("blade"))
+//                    {
+//                        state++;
+//                    }
+//                    break;
+//                case 1:
+//                    foreach (Item val in inventory)
+//                    {
+//                        if (val.Name.Equals("vorpal blade"))
+//                        {
+//                            placeholder = val;
+//                            break;
+//                        }
+//                    }
+//                    if (placeholder != null)
+//                    {
+//                        state++;
+//                    }
+//                    break;
+//                case 2:
+//                    if (hasGiven() == true) //if player uses give comand change given to 1 and return on hasGiven check true add Quest giver to the paramater in hasGiven
+//                    {
 
-                    }
-                    break;
+//                        //update journal to complete and award EXP
 
-            }
-        }
-        public bool hasGiven()
-        {
-            bool given = false;
-            Item placeholder = null;
-            foreach (Item val in Creature.inventory)
-            {
-                if (val.Name.Equals("vorpal blade"))
-                {
-                    placeholder = val;
-                    break;
-                }
-            }
-            if (placeholder != null)
-            {
-                given = true;
-            }
-            return given;
-        }
-    }
-}
+//                    }
+//                    break;
+
+//            }
+//        }
+//        public bool hasGiven()
+//        {
+//            bool given = false;
+//            Item placeholder = null;
+//            foreach (Item val in Creature.)
+//            {
+//                if (val.Name.Equals("vorpal blade"))
+//                {
+//                    placeholder = val;
+//                    break;
+//                }
+//            }
+//            if (placeholder != null)
+//            {
+//                given = true;
+//            }
+//            return given;
+//        }
+//    }
+//}
 
 
 
