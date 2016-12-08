901fae9f8eeae8999617e307139c2cc3bef8e9b8
Jgay90 <19jgay90@gmail.com>
2016-12-08 10:15:34 -0500

    Got last minute tweek done

--- EndofClassProject/EndofClassProject/QuestLog.cs
+++ EndofClassProject/EndofClassProject/QuestLog.cs
@@ -22,15 +22,16 @@ namespace EndofClassProject
         {
             InitializeComponent();
             Quest = quest;
-            foreach(Quest val in quest)
+            foreach (Quest val in quest)
             {
                 questLogListBox.Items.Add(val.questName);
                 val.checkQuest();
-                
+
             }
+            questLogListBox.SelectedIndex = 0;
             if (questLogListBox.SelectedIndex == 0)
             {
-                if (quest[0].state.Equals(1))
+                if (quest[0].state == 1)
                 {
                     questLogTextBox.Text = quest[0].description;
                 }
@@ -39,6 +40,17 @@ namespace EndofClassProject
                     questLogTextBox.Text = "I don't have that Quest yet";
                 }
             }
+            else if (questLogListBox.SelectedIndex == 1)
+            {
+                if (quest[1].state == 1)
+                {
+                    questLogTextBox.Text = quest[1].description;
+                }
+                else
+                {
+                    questLogTextBox.Text = "I don't have that Quest yet";
+                }
+    }
             else if (questLogListBox.SelectedIndex == 1)
             {
                  if (quest[1].state.Equals(1))
